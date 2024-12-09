4d1d939f5576be0a33e6876fc10cf79161d0979e
ruban <kavitharuban3002@gmail.com>
2024-12-09 16:19:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ES6 from './components/ES6';
+
+function App() {
+  return (
+  <ES6/>
+  );
+}
+
+export default App;
---
+++ src/components/ES6.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ES6() {
+  return (
+    <div>
+      <h1>Ruban</h1>
+      <h5></h5>
+    </div>
+  )
+}
+
+export default ES6
